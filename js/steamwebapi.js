f26adbdc181bcaed0b7309c79d87a9507d36eeb4
PlasmaDS <bruno_1849@hotmail.com>
2023-05-10 19:24:54 -0300

    Update steamwebapi.js
    Cambio de API e implementación de  CORS Anywhere.

--- js/steamwebapi.js
+++ js/steamwebapi.js
@@ -1,9 +1,9 @@
 function getStats() {
   const steamId = document.getElementById("steam-id").value;
-  const apiKey = "DF49C4D882FE9530A497F7AFD86D196F";
-  const url = `https://api.steampowered.com/ISteamUserStats/GetUserStatsForGame/v0002/?appid=730&key=${apiKey}&steamid=${steamId}`;
+  const apiKey = "ae941d3c-598a-4fde-9b58-766e999f9c72";
+  const url = `https://cors-anywhere.herokuapp.com/https://public-api.tracker.gg/v2/csgo/standard/profile/steam/${steamId}?TRN-Api-Key=${apiKey}`;
   fetch(url, {
-    mode: 'cors'
+    mode: "cors",
   })
     .then(response => {
       if (response.ok) {
@@ -12,13 +12,12 @@ function getStats() {
         throw new Error("Error en la respuesta de la API");
       }
     })
-    .then(data => {
-      const stats = data.playerstats.stats;
-      const totalKills = stats.find(stat => stat.name === "total_kills").value;
-      const totalDeaths = stats.find(stat => stat.name === "total_deaths").value;
-      const totalRoundsPlayed = stats.find(stat => stat.name === "total_rounds_played").value;
-      const kdRatio = totalKills / totalDeaths;
-      const winPercentage = (stats.find(stat => stat.name === "total_wins").value / stats.find(stat => stat.name === "total_matches_played").value) * 100;
+    .then((response) => {
+      const totalKills = response.data.segments[0].stats.kills.value;
+      const totalDeaths = response.data.segments[0].stats.deaths.value;
+      const totalRoundsPlayed = response.data.segments[0].stats.roundsPlayed.value;
+      const kdRatio = response.data.segments[0].stats.kd.value;
+      const winPercentage = (response.data.segments[0].stats.wins.value / response.data.segments[0].stats.roundsPlayed.value) * 100;
       const html = `<p>Kills totales: ${totalKills}</p>
                     <p>Muertes totales: ${totalDeaths}</p>
                     <p>Rondas jugadas: ${totalRoundsPlayed}</p>
