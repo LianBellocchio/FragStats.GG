e81c428dbfe7282f99735609d9f173e9ec28eb76
LianBellocchio <giulianonicolasbellocchio@gmail.com>
2023-05-11 20:24:38 -0300

    Estilo estadisticas

--- js/steamwebapi.js
+++ js/steamwebapi.js
@@ -1,6 +1,6 @@
 function getStats() {
   const steamId = document.getElementById("steam-id").value;
-  const apiKey = "ae941d3c-598a-4fde-9b58-766e999f9c72";
+  const apiKey = "ae941d3c-598a-4fde-9b58-766e999f9c72"; /* Tracker.gg apikey */
   const url = `https://cors-anywhere.herokuapp.com/https://public-api.tracker.gg/v2/csgo/standard/profile/steam/${steamId}?TRN-Api-Key=${apiKey}`;
   fetch(url, {
     mode: "cors",
@@ -18,11 +18,48 @@ function getStats() {
       const totalRoundsPlayed = response.data.segments[0].stats.roundsPlayed.value;
       const kdRatio = response.data.segments[0].stats.kd.value;
       const winPercentage = response.data.segments[0].stats.wlPercentage.value;
-      const html = `<p>Kills totales: ${totalKills}</p>
-                    <p>Muertes totales: ${totalDeaths}</p>
-                    <p>Rondas jugadas: ${totalRoundsPlayed}</p>
-                    <p>K/D ratio: ${kdRatio.toFixed(2)}</p>
-                    <p>Porcentaje de victorias: ${winPercentage.toFixed(2)}%</p>`;
+
+      const maxKills = 999999; // Valor máximo para las kills totales
+      const maxDeaths = 999999; // Valor máximo para las muertes totales 
+      const maxKDRatio = 100; // Valor máximo para las kills totales
+      const maxRounds = 999999; // Valor máximo para las muertes totales 
+
+      const html = `  <div id="stats">
+      <div class="stat-item">
+        <p>Kills totales: ${totalKills}</p>
+        <div class="progress-bar">
+          <div class="progress" style="width: ${calculatePercentage(totalKills, maxKills)}%"></div>
+        </div>
+      </div>
+  
+      <div class="stat-item">
+        <p>Muertes totales: ${totalDeaths}</p>
+        <div class="progress-bar">
+          <div class="progress" style="width: ${calculatePercentage(totalDeaths, maxDeaths)}%"></div>
+        </div>
+      </div>
+  
+      <div class="stat-item">
+        <p>Rondas jugadas: ${totalRoundsPlayed}</p>
+        <div class="progress-bar">
+          <div class="progress" style="width: ${calculatePercentage(totalRoundsPlayed, maxRounds)}%"></div>
+        </div>
+      </div>
+  
+      <div class="stat-item">
+        <p>K/D ratio: ${kdRatio.toFixed(2)}</p>
+        <div class="progress-bar">
+          <div class="progress" style="width: ${calculatePercentage(kdRatio, maxKDRatio)}%"></div>
+        </div>
+      </div>
+  
+      <div class="stat-item">
+        <p>Porcentaje de victorias: ${winPercentage.toFixed(2)}%</p>
+        <div class="progress-bar">
+          <div class="progress" style="width: ${winPercentage}%"></div>
+        </div>
+      </div>
+    </div>`;
       document.getElementById("stats").innerHTML = html;
     })
     .catch(error => {
@@ -30,3 +67,9 @@ function getStats() {
       document.getElementById("stats").innerHTML = "<p>Error al obtener estadísticas del jugador</p>";
     });
 }
+
+function calculatePercentage(value, max) {
+  return (value / max) * 100;
+}
+
+
