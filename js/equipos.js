6579e67b415f84e89d4881a0d9381782dcfee4df
Bruno Del Sarto <bruno_1849@hotmail.com>
2023-05-17 20:59:17 -0300

    Mostrar/Ocultar Tabla de Equipos

--- js/equipos.js
+++ js/equipos.js
@@ -1,45 +1,56 @@
-function mostrarRanking() {
-  const apiKey = "a1833a7be9fa4e6db2b543e6a4f7759f";
-  const url = `https://api.sportsdata.io/v3/csgo/scores/json/Competitions?key=a1833a7be9fa4e6db2b543e6a4f7759f`;
+let cargado = false;
+const boton = document.querySelector("#button_equipos");
+const tabla = document.querySelector("#tabla-competiciones");
 
-  fetch(url)
-    .then((respuesta) => {
-      if (!respuesta.ok) {
-        throw new Error(
-          `No se pudo obtener los datos de competiciones: ${respuesta.status}`
-        );
-      }
-      return respuesta.json();
-    })
-    .then((datos) => {
-      const tabla = document.getElementById("tabla-competiciones");
-      const thead = tabla.querySelector("thead");
-      const tbody = tabla.querySelector("tbody");
-      tbody.innerHTML = ""; // Limpiar el cuerpo de la tabla
+async function mostrarRanking() {
+  const esVisible = tabla.style.display === "none";
+  if (!cargado) {
+    obtenerDatos();
+  } else {
+    tabla.style.display = esVisible ? "" : "none";
+    boton.innerHTML = esVisible ? "Ocultar Ranking" : "Ver Ranking";
+  }
+}
 
-      // Mostrar el encabezado de la tabla
-      thead.removeAttribute("hidden");
+async function obtenerDatos() {
+  try {
+    const apiKey = "a1833a7be9fa4e6db2b543e6a4f7759f";
+    const url = `https://api.sportsdata.io/v3/csgo/scores/json/Competitions?key=${apiKey}`;
+    const respuesta = await fetch(url);
+    if (!respuesta.ok) {
+      throw new Error(
+        `No se pudo obtener los datos de competiciones: ${respuesta.status}`
+      );
+    }
+    const datos = await respuesta.json();
+    const thead = tabla.querySelector("thead");
+    const tbody = tabla.querySelector("tbody");
+    tbody.innerHTML = ""; // Limpiar el cuerpo de la tabla
 
-      datos.forEach((competicion) => {
-        const fila = document.createElement("tr");
+    // Mostrar el encabezado de la tabla
+    thead.removeAttribute("hidden");
 
-        const columnas = [competicion.AreaName, competicion.Name];
+    datos.forEach((competicion) => {
+      const fila = document.createElement("tr");
+      const columnas = [competicion.AreaName, competicion.Name];
 
-        columnas.forEach((columna) => {
-          const celda = document.createElement("td");
-          celda.textContent = columna;
-          fila.appendChild(celda);
-        });
-
-        // Agregar una nueva celda con un puntaje aleatorio
-        const puntaje = Math.floor(Math.random() * 100 + 1000);
-        const celdaPuntaje = document.createElement("td");
-        celdaPuntaje.textContent = puntaje;
-        celdaPuntaje.setAttribute("data-alias", "puntaje");
-        fila.appendChild(celdaPuntaje);
-
-        tbody.appendChild(fila);
+      columnas.forEach((columna) => {
+        const celda = document.createElement("td");
+        celda.textContent = columna;
+        fila.appendChild(celda);
       });
-    })
-    .catch((error) => console.error(error));
+
+      // Agregar una nueva celda con un puntaje aleatorio
+      const puntaje = Math.floor(Math.random() * 100 + 1000);
+      const celdaPuntaje = document.createElement("td");
+      celdaPuntaje.textContent = puntaje;
+      celdaPuntaje.setAttribute("data-alias", "puntaje");
+      fila.appendChild(celdaPuntaje);
+      tbody.appendChild(fila);
+    });
+  } catch (error) {
+    console.error(error);
+  }
+  boton.innerHTML = "Ocultar Ranking";
+  cargado = true;
 }
