3ad18d059b090d93192db092868af9efdcef2dcd
LianBellocchio <giulianonicolasbellocchio@gmail.com>
2023-05-25 18:41:35 -0300

    Formulario funcionando

--- js/form-validation.js
+++ js/form-validation.js
@@ -53,7 +53,8 @@ document.getElementById("myForm").addEventListener("submit", function (event) {
   if (errorName.textContent === "") {
     // Envía el email usando EmailJS
     emailjs
-      .send("service_ID", "template_ID", {
+      .send("service_f5mlb66", "template_2qkgf4n", {
+        //service_ID y template_ID
         from_name: name,
         from_email: email,
         message_html: message,
--- js/steamwebapi.js
+++ js/steamwebapi.js
@@ -2,9 +2,8 @@ function getStats(input) {
   const steamId = document.querySelector(input).value;
   const apiKey = "ae941d3c-598a-4fde-9b58-766e999f9c72"; /* Tracker.gg apikey */
   const url = `https://cors-anywhere.herokuapp.com/https://public-api.tracker.gg/v2/csgo/standard/profile/steam/${steamId}?TRN-Api-Key=${apiKey}`;
-  fetch(url, {
-    mode: "cors",
-  })
+
+  fetch(url, { mode: "cors" })
     .then((response) => {
       if (response.ok) {
         return response.json();
@@ -13,67 +12,39 @@ function getStats(input) {
       }
     })
     .then((response) => {
-      const totalKills = response.data.segments[0].stats.kills.value;
-      const totalDeaths = response.data.segments[0].stats.deaths.value;
-      const totalRoundsPlayed =
-        response.data.segments[0].stats.roundsPlayed.value;
-      const kdRatio = response.data.segments[0].stats.kd.value;
-      const winPercentage = response.data.segments[0].stats.wlPercentage.value;
+      const statsContainer = document.getElementById("stats");
+      statsContainer.innerHTML = "";
 
-      const maxKills = 999999; // Valor máximo para las kills totales
-      const maxDeaths = 999999; // Valor máximo para las muertes totales
-      const maxKDRatio = 100; // Valor máximo para las kills totales
-      const maxRounds = 999999; // Valor máximo para las muertes totales
+      const stats = response.data.segments[0].stats;
+      const statsKeys = Object.keys(stats);
 
-      const html = `  <div id="stats">
-      <div class="stat-item">
-        <p>Kills totales: ${totalKills}</p>
-        <div class="progress-bar">
-          <div class="progress" style="width: ${calculatePercentage(
-            totalKills,
-            maxKills
-          )}%"></div>
-        </div>
-      </div>
-  
-      <div class="stat-item">
-        <p>Muertes totales: ${totalDeaths}</p>
-        <div class="progress-bar">
-          <div class="progress" style="width: ${calculatePercentage(
-            totalDeaths,
-            maxDeaths
-          )}%"></div>
-        </div>
-      </div>
-  
-      <div class="stat-item">
-        <p>Rondas jugadas: ${totalRoundsPlayed}</p>
-        <div class="progress-bar">
-          <div class="progress" style="width: ${calculatePercentage(
-            totalRoundsPlayed,
-            maxRounds
-          )}%"></div>
-        </div>
-      </div>
-  
-      <div class="stat-item">
-        <p>K/D ratio: ${kdRatio.toFixed(2)}</p>
-        <div class="progress-bar">
-          <div class="progress" style="width: ${calculatePercentage(
-            kdRatio,
-            maxKDRatio
-          )}%"></div>
-        </div>
-      </div>
-  
-      <div class="stat-item">
-        <p>Porcentaje de victorias: ${winPercentage.toFixed(2)}%</p>
-        <div class="progress-bar">
-          <div class="progress" style="width: ${winPercentage}%"></div>
-        </div>
-      </div>
-    </div>`;
-      document.getElementById("stats").innerHTML = html;
+      statsKeys.forEach((statKey) => {
+        const statValue = stats[statKey].value;
+        const statMaxValue = stats[statKey].maxValue;
+
+        const progressPercentage = (statValue / statMaxValue) * 100;
+
+        const circularProgress = document.createElement("div");
+        circularProgress.className = "circular-progress";
+
+        const progress = document.createElement("div");
+        progress.className = "progress";
+        progress.style.transform = `rotate(${progressPercentage * 3.6}deg)`;
+
+        const progressValue = document.createElement("span");
+        progressValue.className = "progress-value";
+        progressValue.textContent = `${progressPercentage.toFixed(0)}%`;
+
+        circularProgress.appendChild(progress);
+        circularProgress.appendChild(progressValue);
+
+        const statItem = document.createElement("div");
+        statItem.className = "stat-item";
+        statItem.innerHTML = `<p class="stat-label">${stats[statKey].displayName}: ${statValue}</p>`;
+        statItem.appendChild(circularProgress);
+
+        statsContainer.appendChild(statItem);
+      });
     })
     .catch((error) => {
       console.error(error);
@@ -81,27 +52,3 @@ function getStats(input) {
         "<p>Error al obtener estadísticas del jugador</p>";
     });
 }
-
-function calculatePercentage(value, max) {
-  return (value / max) * 100;
-}
-
-const input = document.querySelector("#search-bar");
-const estadisticas = document.getElementById("estadisticas");
-input.addEventListener("keypress", function (event) {
-  if (event.key === "Enter") {
-    event.preventDefault();
-    getStats("#search-bar");
-    setTimeout(function () {
-      estadisticas.scrollIntoView({ behavior: "smooth" });
-    }, 1000);
-  }
-});
-
-const lupa = document.querySelector("#lupa");
-lupa.addEventListener("click", () => {
-  getStats("#search-bar");
-  setTimeout(function () {
-    estadisticas.scrollIntoView({ behavior: "smooth" });
-  }, 1000);
-});
