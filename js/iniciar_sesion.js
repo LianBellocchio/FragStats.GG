514b4f7c1d5ed7f4e21bde7550a228922c2472d8
Bruno Del Sarto <bruno_1849@hotmail.com>
2023-07-09 16:55:29 -0300

    Iniciar sesión

---
+++ js/iniciar_sesion.js
@@ -0,0 +1,34 @@
+var usuario = "";
+
+function iniciarSesion(correoBuscado, contrasenaIngresada) {
+  fetch("http://plasmads.pythonanywhere.com/cuentas")
+    .then((response) => response.json())
+    .then((data) => {
+      const objetoEncontrado = data.find(
+        (objeto) => objeto.correo === correoBuscado
+      );
+
+      if (objetoEncontrado) {
+        if (objetoEncontrado.contrasena === contrasenaIngresada) {
+          alert("Inicio de sesión correcto");
+          return (usuario = correoBuscado);
+        } else {
+          alert("Contraseña incorrecta");
+        }
+      } else {
+        alert("Correo no encontrado");
+      }
+    })
+    .catch((error) => {
+      console.error("Error al realizar la solicitud:", error);
+    });
+}
+
+document
+  .getElementById("botonIniciarSesion")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+    var correo = document.getElementById("correo").value;
+    var contrasena = document.getElementById("contrasena").value;
+    iniciarSesion(correo, contrasena);
+  });
