172b388ea96482ab65634d41f8a54429bd45fcf8
LianBellocchio <giulianonicolasbellocchio@gmail.com>
2023-07-10 16:34:42 -0300

    Favoritos en barra de busqueda

--- js/favoritos.js
+++ js/favoritos.js
@@ -1,4 +1,5 @@
 var favoritos = "";
+var busquedasRecientes = [];
 var ID = localStorage.getItem("id");
 
 function traerFavoritos() {
@@ -28,23 +29,59 @@ function eliminar(favorito) {
 }
 
 function agregar(favorito) {
-  traerFavoritos().then(() => {
-    if (favoritos !== null && favoritos.indexOf(favorito) !== -1) {
-      eliminar(favorito);
-    } else {
-      if (favoritos === "" || favoritos === null) {
-        favoritos = favorito;
-      } else {
-        favoritos += "," + favorito;
-      }
-      modificarFavoritos(1, favoritos);
-    }
-  });
-}
+    traerFavoritos().then(() => {
+      // Realizar búsqueda aquí
+      fetch(`https://api.tracker.gg/v2/search?platform=steam&query=${favorito}`)
+        .then((response) => response.json())
+        .then((data) => {
+          // Verificar si se encontraron resultados de búsqueda
+          if (data.data && data.data.length > 0) {
+            // Obtener el primer resultado de búsqueda (asumiendo que es el más relevante)
+            const resultado = data.data[0];
+            const nombre = resultado.attributes.name;
+            const tipo = resultado.type;
+  
+            // Realizar acciones basadas en el tipo de resultado (jugador, equipo, etc.)
+            if (tipo === "player") {
+              // Acciones para jugador
+              const jugadorId = resultado.id;
+              // Resto del código para agregar o eliminar el favorito
+            } else if (tipo === "team") {
+              // Acciones para equipo
+              const equipoId = resultado.id;
+              // Resto del código para agregar o eliminar el favorito
+            } else {
+              // Otro tipo de resultado
+              console.log("Tipo de resultado no compatible:", tipo);
+            }
+  
+            // Verificar si la búsqueda ya existe en las busquedasRecientes
+            if (!busquedasRecientes.includes(favorito)) {
+              busquedasRecientes.push(favorito);
+            }
+  
+            if (favoritos !== null && favoritos.indexOf(favorito) !== -1) {
+              eliminar(favorito);
+            } else {
+              if (favoritos === "" || favoritos === null) {
+                favoritos = favorito;
+              } else {
+                favoritos += "," + favorito;
+              }
+              modificarFavoritos(1, favoritos);
+            }
+          } else {
+            console.log("No se encontraron resultados de búsqueda");
+          }
+        })
+        .catch((error) => {
+          console.error("Error al realizar la búsqueda:", error);
+        });
+    });
+  }
 
 function modificarFavoritos(id, favoritosModificados) {
   var url = `http://plasmads.pythonanywhere.com/cuentas/${ID}`;
-
   traerFavoritos().then(() => {
     var datosModificados = {
       contrasena: contrasena,
@@ -53,7 +90,6 @@ function modificarFavoritos(id, favoritosModificados) {
       nombre: nombre,
       favoritos: favoritosModificados,
     };
-
     var options = {
       method: "PUT",
       headers: {
@@ -61,7 +97,6 @@ function modificarFavoritos(id, favoritosModificados) {
       },
       body: JSON.stringify(datosModificados),
     };
-
     fetch(url, options)
       .then((response) => {
         if (response.ok) {
@@ -76,14 +111,52 @@ function modificarFavoritos(id, favoritosModificados) {
   });
 }
 
-function obtenerFavoritos() {
-  const arrayFavoritos = favoritos.split(",");
-  return arrayFavoritos;
+function mostrarBusquedasRecientes() {
+  var searchInput = document.getElementById("search-bar");
+  var recentSearchesContainer = document.getElementById("recent-searches");
+
+  // Limpiar las búsquedas recientes anteriores
+  recentSearchesContainer.innerHTML = "";
+
+  // Recorrer el array de busquedasRecientes y crear elementos de lista para cada una
+  busquedasRecientes.forEach((busqueda) => {
+    var listItem = document.createElement("li");
+    var link = document.createElement("a");
+    link.href = "#"; // Agrega la acción que deseas para redirigir a la búsqueda
+
+    // Establecer el texto de la búsqueda en el enlace
+    link.textContent = busqueda;
+
+    // Agregar un evento de clic al enlace para agregar la búsqueda a favoritos
+    link.addEventListener("click", function (event) {
+      event.preventDefault();
+      agregar(busqueda);
+    });
+
+    // Agregar el enlace al elemento de lista y el elemento de lista al contenedor de búsquedas recientes
+    listItem.appendChild(link);
+    recentSearchesContainer.appendChild(listItem);
+  });
+
+  // Mostrar el contenedor de búsquedas recientes
+  recentSearchesContainer.style.display = "block";
 }
 
-document
-  .getElementById("guardarFavorito")
-  .addEventListener("click", function (event) {
-    event.preventDefault();
-    agregar(document.getElementById("favorito").value);
-  });
+document.getElementById("lupa").addEventListener("click", function (event) {
+  event.preventDefault();
+  mostrarBusquedasRecientes();
+});
+
+document.getElementById("search-bar").addEventListener("focus", function () {
+  mostrarBusquedasRecientes();
+});
+
+document.getElementById("search-bar").addEventListener("blur", function () {
+  var recentSearchesContainer = document.getElementById("recent-searches");
+  recentSearchesContainer.style.display = "none";
+});
+
+document.getElementById("guardarFavorito").addEventListener("click", function (event) {
+  event.preventDefault();
+  agregar(document.getElementById("favorito").value);
+});
